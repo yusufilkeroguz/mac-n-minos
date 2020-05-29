96ba30366b1c9daf54768ea21d63cc926613ae2b
yusufilkeroguz <ilker.oguz@modanisa.com>
2020-05-29 20:31:51 +0300

    Server side rendering and redux added.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Loadable from 'react-loadable';
+import { Provider as ReduxProvider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import configureStore from './store/configureStore';
+// import registerServiceWorker from './registerServiceWorker';
+
+const store = configureStore( window.__REDUX_STATE__ || {} );
+
+const AppBundle = (
+    <ReduxProvider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ReduxProvider>
+);
+
+window.onload = () => {
+    Loadable.preloadReady().then(() => {
+        ReactDOM.hydrate(
+            AppBundle,
+            document.getElementById('root')
+        );
+    });
+};
